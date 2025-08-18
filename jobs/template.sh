2974be3692b77980edbc7ce91ef2953e3e668149
Luan Leal <luanric888@gmail.com>
2025-08-18 16:06:21 -0300

    Update
    Added `/data` to `.gitignore`
    Updated diamond blastp

---
+++ jobs/template.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+#SBATCH −−job−name=DMND_BENCHMARK 
+#SBATCH −−ntasks=8
+#SBATCH −−time=16:00:00 
+
+export PATH="/home/lleal/.pixi/bin:$PATH"
+
+echo -e "\n## Job iniciado em $(date +'%d-%m-%Y as %T') #####################\n"
+
+cd $SLURM_SUBMIT_DIR
+
+bash script.sh
