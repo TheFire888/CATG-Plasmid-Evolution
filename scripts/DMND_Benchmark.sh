cf160729791b6461a1b88b69c41ded4e957c813d
Luan Leal <luanric888@gmail.com>
2025-08-19 11:54:26 -0300

    update
    backslash was wrong

--- scripts/DMND_Benchmark.sh
+++ scripts/DMND_Benchmark.sh
@@ -10,23 +10,23 @@ for i in $(seq 1 10); do
     echo "Executando com X = $X (max-target-seqs = $MAX_TARGET)..."
 
     START_TIME=$(date +%s.%N)
-    pixi run diamond blastp \ 
-        -q "data/gene_sample.faa" \ 
-        -d "data/DMND_DB" \ 
-        --multiprocessing \ 
-        --mp-init \ 
-        --tmpdir "$TMPDIR" \ 
+    pixi run diamond blastp \
+        -q "data/gene_sample.faa" \
+        -d "data/DMND_DB" \
+        --multiprocessing \
+        --mp-init \
+        --tmpdir "$TMPDIR" \
         --parallel-tmpdir "$PTMPDIR"
 
-    pixi run diamond blastp \ 
-        -q "data/gene_sample.faa" \ 
-        -d "data/DMND_DB" \ 
-        -o "test/DMND_BlastP_$i.tsv" \ 
-        --ultra-sensitive \ 
-        --outfmt 6 qseqid sseqid pident qcovhsp scovhsp bitscore \ 
-        --max-target-seqs "$MAX_TARGET" \ 
-        --multiprocessing \ 
-        --tmpdir "$TMPDIR" \ 
+    pixi run diamond blastp \
+        -q "data/gene_sample.faa" \
+        -d "data/DMND_DB" \
+        -o "test/DMND_BlastP_$i.tsv" \
+        --ultra-sensitive \
+        --outfmt 6 qseqid sseqid pident qcovhsp scovhsp bitscore \
+        --max-target-seqs "$MAX_TARGET" \
+        --multiprocessing \
+        --tmpdir "$TMPDIR" \
         --parallel-tmpdir "$PTMPDIR"
     END_TIME=$(date +%s.%N)
 
