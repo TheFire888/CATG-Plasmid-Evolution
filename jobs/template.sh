1d95f54b01ae79cb191fa647a6dceff3a97285b4
Luan Leal <luanric888@gmail.com>
2025-08-19 08:51:52 -0300

    new template

--- jobs/template.sh
+++ jobs/template.sh
@@ -1,13 +1,18 @@
 #/bin/bash
 
-#SBATCH −−job−name=DMND_BENCHMARK 
-#SBATCH −−ntasks=8
-#SBATCH −−time=16:00:00 
+#SBATCH --job-name=DMND-benchmark
+#SBATCH --ntasks=16
+#SBATCH --time=32:00:00
+#SBATCH --output=DMNDbench_job.out
+#SBATCH --error=DMNDbench_job.err
+
+SCRATCH="/scratch/local"
 
 export PATH="/home/lleal/.pixi/bin:$PATH"
 
-echo -e "\n## Job iniciado em $(date +'%d-%m-%Y as %T') #####################\n"
 
-cd $SLURM_SUBMIT_DIR
+echo -e "\n## Job iniciado em $(date +'%d-%m-%Y as %T') ##\n"
+
+cd "$SLURM_SUBMIT_DIR/.."
 
 bash script.sh
