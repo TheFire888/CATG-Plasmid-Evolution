6616e17b04a11993eda503bdb9eeac093d8c28db
Luan Leal <luanric888@gmail.com>
2025-08-22 15:07:23 -0300

    uodate

--- scripts/DMND_Benchmark.sh
+++ scripts/DMND_Benchmark.sh
@@ -5,31 +5,31 @@ PLASMID_COUNT=5583
 
 > "$LOGFILE"
 
-for i in $(seq 2 10); do
+for i in $(seq 1 10); do
     X=$(awk "BEGIN {print $i * 0.1}")
     MAX_TARGET=$(awk "BEGIN {print int($PLASMID_COUNT * $X)}")
 
     echo "Executando com X = $X (max-target-seqs = $MAX_TARGET)..."
 
     START_TIME=$(date +%s.%N)
-    pixi run diamond blastp \
-        -q "data/gene_sample.faa" \
-        -d "data/DMND_DB" \
-        --multiprocessing \
-        --mp-init \
-        --tmpdir "$TMPDIR" \
-        --parallel-tmpdir "$PTMPDIR"
+    #pixi run diamond blastp \
+    #    -q "data/gene_sample.faa" \
+    #    -d "data/DMND_DB" \
+    #    --multiprocessing \
+    #    --mp-init \
+    #    --tmpdir "$TMPDIR" \
+    #    --parallel-tmpdir "$PTMPDIR"
 
     pixi run diamond blastp \
         -q "data/gene_sample.faa" \
         -d "data/DMND_DB" \
-        -o "test/DMND_BlastP_$i.tsv" \
+        -o "test/DMND_BlastP_03_$i.tsv" \
         --very-sensitive \
-        --outfmt 6 qseqid sseqid pident qcovhsp scovhsp bitscore \
+        --outfmt 6 qseqid sseqid pident bitscore \
         --max-target-seqs "$MAX_TARGET" \
-        --multiprocessing \
-        --tmpdir "$TMPDIR" \
-        --parallel-tmpdir "$PTMPDIR"
+    #    --multiprocessing \
+    #    --tmpdir "$TMPDIR" \
+    #    --parallel-tmpdir "$PTMPDIR"
     #PID_DIAMOND=$!
 
     #bash mem_profiler.sh "$PID_DIAMOND" "diamond_mem_usage_$X.log" &
