792dc3d9f58c7b2021b551142e2a5665e7238ad9
Luan Leal <luanric888@gmail.com>
2025-09-12 17:50:11 -0300

    update

---
+++ jobs/process-data-test-001.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Esse nome vai aparecer na execução do squeue
+#SBATCH --job-name=process-data-test-001
+
+# Esse é o número de processos
+#SBATCH --ntasks=1
+
+# Essa é a memória que seu processo pode usar
+#SBATCH --mem=32GB
+
+# Esse é o número de cpus
+#SBATCH --cpus-per-task=4
+
+# Essa é a estimativa de tempo. NOTE que seu processo será encerrado após esse tempo limite
+#SBATCH --time=2:00:00
+
+# Esses são os arquivos de saída do StdOut e do StdErr, respectivamente
+#SBATCH --output=process-data-test-001.out
+#SBATCH --error=process-data-test-001.err
+
+export PATH="/home/lleal/.pixi/bin:$PATH"
+
+echo -e "\n## Job iniciado em $(date +'%d-%m-%Y as %T') ##\n"
+
+cd "$SLURM_SUBMIT_DIR/../test/biotmp-qcMTC/"
+pixi run python ../../src/proteomic/process_data.py eval gene_count.tsv M5-Graph.ftree filtered_DMBD_BlastP.tsv
