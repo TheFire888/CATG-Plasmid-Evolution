493cfd9dbf9732b8de2b907bcf09749f983eba85
Luan Leal <luanric888@gmail.com>
2025-09-19 23:22:14 -0300

    update job

--- jobs/plasmidEvo.sh
+++ jobs/plasmidEvo.sh
@@ -11,11 +11,11 @@ export PATH="/home/lleal/.pixi/bin:$PATH"
 
 echo -e "\n## Job ${SLURM_JOB_ID} iniciado em $(date +'%d-%m-%Y as %T') ##\n"
 
-WORKDIR="test/${SLURM_JOB_ID}.$(date +'$d-$m-$Y_%T')"
+WORKDIR="test/${SLURM_JOB_ID}.$(date +'%d-%m-%Y_%T')"
 mkdir ${WORKDIR}
 SAMPLE_PATH="${WORKDIR}/sample.fna" 
 
 pixi run seqkit sample -p 0.2 "data/genbank_plasmid_seqs.fna" > ${SAMPLE_PATH}
-pixi run plasmid-evo ${WORKDIR} ${SAMPLE_PATH}
+pixi run plasmid-evo ${SAMPLE_PATH} ${WORKDIR}
 
 
