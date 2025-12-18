b56afeee23f1b8d9a9d8a4d7ff48943cac6e5814
Luan Leal <luanric888@gmail.com>
2025-12-18 10:14:29 -0300

    new job to cluster proteins

---
+++ jobs/protein_clustering.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=protein_clu
+#SBATCH --partition=max50
+#SBATCH --ntasks=1
+#SBATCH --mem=18GB
+#SBATCH --cpus-per-task=16
+#SBATCH --time=32:00:00
+#SBATCH --output=out/protein_clu%j.out
+#SBATCH --error=err/protein_clu%j.err
+
+export PATH="/home/lleal/.pixi/bin:$PATH"
+
+echo -e "\n## Job ${SLURM_JOB_ID} iniciado em $(date +'%d-%m-%Y as %T') ##\n"
+
+WORKDIR="test/6790.17-12-2025_10:51:03"
+
+awk -v OFS='\t' '{print $1, $2, $4}' "${WORKDIR}/diamond_results.tsv" > "$WORKDIR}/diamond_results_filtered.tsv"
+pixi run diamond --threads 16 \
+    --out "${WORKDIR}/diamond_protein_clustering" \
+    --header "simple" \
+    --edge-format "triplet" \
+    --edges "${WORKDIR}/diamond_results_filtered.tsv"
+
