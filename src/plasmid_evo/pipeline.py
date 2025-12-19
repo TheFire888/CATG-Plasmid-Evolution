6d10e5464937c1a2364f5c760e0f0c027821ffc7
Luan Leal <luanric888@gmail.com>
2025-12-19 10:51:37 -0300

    added memory usage logs

--- src/plasmid_evo/pipeline.py
+++ src/plasmid_evo/pipeline.py
@@ -2,6 +2,10 @@
 Este módulo realiza toda a pipeline necessária para o plasmidEvo
 """
 from pathlib import Path
+import psutil
+import threading
+import time
+import os
 import logging
 logging.basicConfig(
         level=logging.DEBUG,
@@ -48,6 +52,17 @@ class PlasmidEvoPipeline:
         logging.info("Iniciando pipeline plasmidEvo")
         output_path = Path(output_dir)
         output_path.mkdir(parents=True, exist_ok=True)
+
+        def log_memory():
+            process = psutil.Process(os.getpid())
+            while True:
+                mem = process.memory_info().rss / 1024**2
+                logging.debug(f"Monitor: {mem:.2f} MB em uso")
+                time.sleep(10)
+
+        # Thread secundária para monitoramento de memória
+        # Talvez exiga um segundo processo? Preciso ver isso aí, vou assumir que não
+        threading.Thread(target=log_memory, daemon=True).start()
 
         self.predictor.predict(input_fasta, output_path)
         self.aligner.align(output_path)
