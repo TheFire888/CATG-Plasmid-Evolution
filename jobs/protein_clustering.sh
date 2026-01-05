d39a80b8f4dd76f8fcb94417ab6eda49e655906d
Luan Leal <luanric888@gmail.com>
2026-01-05 10:21:28 -0300

    new protein clustering job, now fixed

--- jobs/protein_clustering.sh
+++ jobs/protein_clustering.sh
@@ -12,19 +12,19 @@ export PATH="/home/lleal/.pixi/bin:$PATH"
 
 echo -e "\n## Job ${SLURM_JOB_ID} iniciado em $(date +'%d-%m-%Y as %T') ##\n"
 
-WORKDIR="test/6790.17-12-2025_10:51:03"
+WORKDIR="test/6866.19-12-2025_18:13:19"
 
-awk -v OFS='\t' '{print $1, $2, 1}' "${WORKDIR}/diamond_results.tsv" > "${WORKDIR}/diamond_results_filtered.tsv"
+awk -v OFS='\t' '{print $1, $2, 1}' "${WORKDIR}/rbh_hits.tsv" > "${WORKDIR}/proteins_edges.tsv"
 
 awk '{ if (!seen[$1]++) print $1; if (!seen[$2]++) print $2 }' "${WORKDIR}/diamond_results.tsv" > "${WORKDIR}/proteins_list.txt"
 
 pixi run diamond greedy-vertex-cover \
     --verbose \
-    --log \
+    --connected-component-depth 0 \
     --threads 16 \
     --db "${WORKDIR}/proteins_list.txt" \
-    --out "${WORKDIR}/diamond_protein_clustering" \
+    --out "${WORKDIR}/diamond_protein_clustering.tsv" \
     --header "simple" \
     --edge-format "triplet" \
-    --edges "${WORKDIR}/diamond_results_filtered.tsv"
+    --edges "${WORKDIR}/proteins_edges.tsv"
 
