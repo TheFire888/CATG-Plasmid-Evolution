811ef322fced759c80d6d3cc0a457ac6e6082f71
Luan Leal <luanric888@gmail.com>
2026-01-12 16:53:42 -0300

    added rbh filtering job and script

---
+++ jobs/rbh_filtering.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=rbh_filter
+#SBATCH --partition=max50
+#SBATCH --ntasks=1
+#SBATCH --mem=32GB
+#SBATCH --cpus-per-task=16
+#SBATCH --time=100:00:00
+#SBATCH --output=out/rbh_filter%j.out
+#SBATCH --error=err/rbh_filter%j.err
+
+export PATH="/home/lleal/.pixi/bin:$PATH"
+
+echo -e "\n## Job ${SLURM_JOB_ID} iniciado em $(date +'%d-%m-%Y as %T') ##\n"
+
+WORKDIR="/home/lleal/programs/plasmidEvo/rslts"
+SCRIPTSDIR="scripts/"
+
+pixi run python "${SCRIPTSDIR}/rbh_filter.py" "${WORKDIR}"
---
+++ scripts/rbh_filter.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+import threading
+import psutil
+import time
+import os
+import logging
+logging.basicConfig(
+        level=logging.DEBUG,
+        format="{asctime} - {levelname} - {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M",
+        )
+
+import polars as pl
+import click
+
+def log_memory():
+    process = psutil.Process(os.getpid())
+    while True:
+        mem = process.memory_info().rss / 1024**2
+        logging.debug(f"Monitor: {mem:.2f} MB em uso")
+        time.sleep(60)
+
+threading.Thread(target=log_memory, daemon=True).start()
+
+def diamond_filter(output_dir: Path) -> None:
+    """
+    Aplica o filtro de RBH a um arquivo de hits.
+
+    Este método implementa uma estratégia de self-join em um lazyframe
+    polars do arquivo de entrada para identificar os pares de RBH
+    de forma eficiente.
+
+    Args:
+        output_dir (Path): Diretório onde o arquivo filtrado será salvo.
+    """
+    logging.info("Filtrando Reciprocal Best Hits (RBH)")
+    input_path = output_dir / "diamond_results.tsv"
+    output_path = output_dir / "rbh_hits.tsv"
+
+    lf = pl.scan_csv(
+        input_path,
+        separator='\t',
+        has_header=False,
+        new_columns=['qseq_gene', 'sseq_gene',
+                     'bitscore'
+                     ],
+        schema_overrides=[pl.Categorical, pl.Categorical,
+                          pl.Float64
+                          ]
+    ).with_columns(
+        qseq_contig=(pl.col("qseq_gene").cast(pl.Utf8)
+                     .str.replace(r"_[^_]*$", "").cast(pl.Categorical)),
+        sseq_contig=(pl.col("sseq_gene").cast(pl.Utf8)
+                     .str.replace(r"_[^_]*$", "").cast(pl.Categorical))
+    ).filter(  # Garante que teremos os autohits
+        (pl.col("qseq_contig") != pl.col("sseq_contig"))
+        | (pl.col("qseq_gene") == pl.col("sseq_gene"))
+    ).group_by(
+        "qseq_gene", "sseq_contig"
+    ).first()
+
+    lf.join(
+        lf.select(
+            pl.col("qseq_gene").alias("reverse_qseq"),
+            pl.col("sseq_gene").alias("reverse_sseq")
+        ),
+        left_on=["qseq_gene", "sseq_gene"],
+        right_on=["reverse_sseq", "reverse_qseq"],
+        how="inner"
+    ).select(
+        'qseq_gene', 'sseq_gene', 'bitscore'
+    ).sink_csv(output_path,
+               separator='\t',
+               include_header=False
+               )
+
+    logging.info(f"Filtragem concluída. Pares de RBH salvos em "
+          f"{output_path}")
+    return output_path
+
+@click.command()
+@click.argument('output_dir', type=click.Path(exists=True))
+def main(output_dir):
+    diamond_filter(Path(output_dir))
+
+if __name__ == "__main__":
+    main()
