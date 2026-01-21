391e0ee553291891cababfb7654823cbd71ad657
Luan Leal <luanric888@gmail.com>
2026-01-21 10:32:59 -0300

    added: infomap job

---
+++ jobs/infomap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=infomap
+#SBATCH --partition=max50
+#SBATCH --ntasks=1
+#SBATCH --mem=64GB
+#SBATCH --cpus-per-task=4
+#SBATCH --time=100:00:00
+#SBATCH --output=out/infomap%j.out
+#SBATCH --error=err/infomap%j.err
+
+export PATH="/home/lleal/.pixi/bin:$PATH"
+
+echo -e "\n## Job ${SLURM_JOB_ID} iniciado em $(date +'%d-%m-%Y as %T') ##\n"
+
+WORKDIR="/home/lleal/programs/plasmidEvo/rslts"
+
+pixi run infomap \
+    "${WORKDIR}/graph.net" \
+    "${WORKDIR}" \
+    --seed 888 \
+    --num-trials 1 \
+    --flow-model "undirected" \
+    --two-level \
+    --ftree \
+    -vvv
