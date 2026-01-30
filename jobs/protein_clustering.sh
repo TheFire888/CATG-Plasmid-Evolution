09052d90ad41472fd7078c7f5072522b81e3b31a
Luan Leal <luanric888@gmail.com>
2026-01-30 14:12:02 -0300

    chore: added more resources

--- jobs/protein_clustering.sh
+++ jobs/protein_clustering.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=protein_clu
 #SBATCH --partition=max50
 #SBATCH --ntasks=1
-#SBATCH --mem=64GB
+#SBATCH --mem=128GB
 #SBATCH --cpus-per-task=16
 #SBATCH --time=32:00:00
 #SBATCH --output=out/protein_clu%j.out
@@ -31,13 +31,13 @@ echo -e "\n## Job ${SLURM_JOB_ID} iniciado em $(date +'%d-%m-%Y as %T') ##\n"
 
 WORKDIR="/home/lleal/programs/plasmidEvo/rslts"
 
-awk -v OFS='\t' '{print $1, $2, 1}' "${WORKDIR}/rbh_hits.tsv" > "${WORKDIR}/proteins_edges.tsv"
-
-echo "proteins_edges.tsv done!"
-
-awk '{ if (!seen[$1]++) print $1; if (!seen[$2]++) print $2 }' "${WORKDIR}/rbh_hits.tsv" > "${WORKDIR}/proteins_list.txt"
-
-echo "proteins_list.txt done!"
+# awk -v OFS='\t' '{print $1, $2, 1}' "${WORKDIR}/rbh_hits.tsv" > "${WORKDIR}/proteins_edges.tsv"
+# 
+# echo "proteins_edges.tsv done!"
+# 
+# awk '{ if (!seen[$1]++) print $1; if (!seen[$2]++) print $2 }' "${WORKDIR}/rbh_hits.tsv" > "${WORKDIR}/proteins_list.txt"
+# 
+# echo "proteins_list.txt done!"
 
 pixi run diamond greedy-vertex-cover \
     --verbose \
